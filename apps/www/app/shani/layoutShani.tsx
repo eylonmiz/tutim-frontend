6db4b9cb6de1983f8ae0823d6083cf931be830a2
shani_levi <shanilevi01011@gmail.com>
2023-06-04 14:27:02 +0300

    feat(page): add page name

--- apps/www/app/shani/layoutShani.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function layoutShani() {
-  return <div>SHANI</div>
-}
-
-export default layoutShani
---
+++ apps/www/app/shani/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function page() {
+  return <div>shnai</div>
+}
+
+export default page
